5efaabb2440c620ea0b2d7de295c36f23b04f30f
Immortals00 <92683533+Immortals00@users.noreply.github.com>
2023-03-26 10:50:26 +0530

    Update Category.jsx

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import Card from "./Card";
-const Category = ({ selectedCategory, name, description }) => {
-
+const Category = ({ selectedCategory, name, liteDesc, regDesc, megaDesc }) => {
   return (
     <div className="categoryWrapper">
-      <Card name={name} pack="Lite" />
-      <Card name={name} pack="Regular" />
-      <Card name={name} pack="Mega" />
+      <Card
+        selectedCategory={selectedCategory}
+        name={name}
+        description={liteDesc}
+        pack="Lite"
+      />
+      <Card
+        selectedCategory={selectedCategory}
+        name={name}
+        description={regDesc}
+        pack="Regular"
+      />
+      <Card
+        selectedCategory={selectedCategory}
+        name={name}
+        description={megaDesc}
+        pack="Mega"
+      />
     </div>
   );
 };
